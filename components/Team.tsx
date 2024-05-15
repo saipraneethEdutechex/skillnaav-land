67a1e0d823342b3cc44de0ae3bb3b3c6249341c4
saipraneethEdutechex <saipraneeth.t@edutechex.com>
2024-05-15 13:09:09 +0530

    NAmes CHanged

--- components/Team.tsx
+++ components/Team.tsx
@@ -38,7 +38,7 @@ const Team = () => {
       ],
     },
     {
-      name: "Ms Hem Ranjani",
+      name: "Christopher Jadhav",
       position: "Head, HR & Schools BD",
       description:
         "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
