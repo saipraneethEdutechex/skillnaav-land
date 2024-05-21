7a3475a8856ef07c3c3022c60c5ae5d0f87ab86e
saipraneethEdutechex <saipraneeth.t@edutechex.com>
2024-05-21 11:42:04 +0530

    Favicon Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,18 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "SkillNaav",
   description: "Tailored Internship Platform",
+  icons: {
+    icon: [
+      "/favicon.ico?v=4",
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -17,6 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Your App Title</title>
+      </Head>
       <body className={inter.className}>
         <Navbar />
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,15 @@ import Faq from "@/components/Faq";
 import { Pricing } from "@/components/Pricing";
 import Team from "@/components/Team";
 import { Footer } from "@/components/Footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Home - Your App Title</title>
+        <meta name="description" content="This is the homepage of your app" />
+      </Head>
       <Hero />
       <div className="px-[20px] lg:container lg:px-20 mx-auto">
         <Features />
